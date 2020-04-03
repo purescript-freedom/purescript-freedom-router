538b75410301edb03008e868e265db96dfe51ee1
Shinya Takahashi <s.takahashi313@gmail.com>
2020-04-03 23:46:21 +0900

    Update example

--- example/Main.purs
+++ example/Main.purs
@@ -41,22 +41,22 @@ view :: State -> VNode State
 view state =
   case state of
     Home ->
-      H.keyed "home" $ H.div # H.kids
+      H.key "home" $ H.div # H.kids
         [ H.h1 # H.kids [ H.t "Home" ]
         , link "/users/51" # H.kids [ H.t "To user 51" ]
         ]
     User i ->
-      H.keyed "user" $ H.div # H.kids
+      H.key "user" $ H.div # H.kids
         [ H.h1 # H.kids [ H.t $ "User " <> show i ]
         , link "/users?name=ichiro" # H.kids [ H.t "To ichiro" ]
         ]
     Users name ->
-      H.keyed "users" $ H.div # H.kids
+      H.key "users" $ H.div # H.kids
         [ H.h1 # H.kids [ H.t $ "I am " <> name ]
         , link "/not_found" # H.kids [ H.t "Somewhere" ]
         ]
     NotFound ->
-      H.keyed "notFound" $ H.div # H.kids
+      H.key "notFound" $ H.div # H.kids
         [ H.h1 # H.kids [ H.t "NotFound" ]
         , link "/" # H.kids [ H.t "To Home" ]
         ]
